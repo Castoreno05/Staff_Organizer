01126edc0d395120234f455a678c038e038b1a14
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-08-01 13:53:49 -0500

    Adding routes

--- Main/server.js
+++ Main/server.js
@@ -21,21 +21,58 @@ const db = mysql.createConnection(
     password: "I69@WdHOH)(E1[E",
     database: "staff_db",
   },
-  console.log("You have been connected to the staff_database.")
+  console.log("You have been connected to the staff_db database")
 );
 
-db.query('SELECT * FROM staff_db.department', function(err, results){
-    console.log(results);
+// View all departments
+app.get("/api/departments", (req, res) => {
+    // Returning department names from department table
+  const sql = `SELECT department_name FROM department`;
+  db.query(sql, (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
 });
 
-db.query('SELECT * FROM staff_db.e_role', function (err, res){
-    console.log(res);
+// View all the roles
+app.get("/api/roles", (req, res) => {
+    // Returning roles from the e_role table
+  const sql = `SELECT e_role FROM e_role `;
+  db.query(sql, (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
 });
 
-db.query('SELECT * FROM staff_db.employee', function (err, res){
-    console.log(res);
-});
+// View all employees
+app.get("/api/employees", (req, res) => {
+    // Returning first and last name from employee table
+    const sql = `SELECT first_name, last_name FROM employee`;
+    db.query(sql, (err, row) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: "success",
+        data: row,
+      });
+    });
+  });
+  
 
 app.listen(PORT, () => {
-    console.log(`App Listening at http://localhost:${PORT}`);
+  console.log(`App Listening at http://localhost:${PORT}`);
 });
